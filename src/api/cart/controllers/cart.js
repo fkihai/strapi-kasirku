c8bb6a968e61c97f9914c68a0a33488791f51669
fkihai <fikrihaikal568@gmail.com>
2024-12-31 09:47:00 +0700

    feet: post cart with id user token login

--- src/api/cart/controllers/cart.js
+++ src/api/cart/controllers/cart.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 'use strict';
 
 /**
@@ -6,4 +7,32 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::cart.cart');
+module.exports = createCoreController('api::cart.cart', ({ strapi }) => ({
+  async create(ctx) {
+    const { user } = ctx.state; // Ambil user dari token
+
+    // Pastikan user sudah login
+    if (!user) {
+      return ctx.unauthorized('Anda harus login.');
+    }
+
+    // Ambil data dari request body
+    const { data } = ctx.request.body;
+
+    if (!data || !data.product || !data.quantity) {
+      return ctx.badRequest('Data tidak lengkap.');
+    }
+
+    // Buat data cart dengan relasi ke user
+    const cart = await strapi.entityService.create('api::cart.cart', {
+      data: {
+        users_permissions_user: user.id, // Tambahkan user dari token
+        product: data.product,
+        quantity: data.quantity,
+      },
+    });
+
+    return cart; // Kembalikan data yang dibuat
+  },
+}));
+
